359576154c396be0b8dc8b8ce53f27089960dea0
Jane Jeon <JaneJeon9719@gmail.com>
2018-10-27 04:04:07 -0400

    Implemented background v3 example

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 const path = require("path"),
   request = require("request-promise")
+require("hbs")
 
 require("dotenv").config()
 require("express")()
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "hbs")
   .use(require("body-parser").json())
-  .get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")))
-  .post("/", async (req, res) => {
+  .get("/v2", (req, res) =>
+    res.render("v2", { V2_PUBLIC: process.env.V2_PUBLIC })
+  )
+  .get("/v3", (req, res) =>
+    res.render("v3", { V3_PUBLIC: process.env.V3_PUBLIC })
+  )
+  .post("/v2", async (req, res) => {
     const result = JSON.parse(
       await request
         .post("https://www.google.com/recaptcha/api/siteverify")
         .form({
-          secret: process.env.CAPTCHA_SECRET,
-          response: req.body["g-recaptcha-response"]
+          secret: process.env.V2_PRIVATE,
+          response: req.body["g-recaptcha-response"],
+          remoteip: req.ip
         })
     )
 
-    result.success ? res.status(200).send(result) : res.sendStatus(403)
+    res.send(result)
+  })
+  .post("/v3", async (req, res) => {
+    const result = JSON.parse(
+      await request
+        .post("https://www.google.com/recaptcha/api/siteverify")
+        .form({
+          secret: process.env.V3_PRIVATE,
+          response: req.body.token,
+          remoteip: req.ip
+        })
+    )
+
+    res.send(result)
   })
   .listen(3000, err => {
     if (err) console.error(err)
